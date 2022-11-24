809402b2f7d367dbb8d4b989892668f3b37bcae8
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2022-11-24 16:06:33 -0300

    Classe 03 - Finalizado

--- classe-03/index.js
+++ classe-03/index.js
@@ -1,1 +1,9 @@
 const texto = "Aprenda programar do zero na Cubos Academy";
+
+
+function urlAmigavel(texto) {
+    texto = (texto.split(' ').join('-')).toLowerCase();
+    console.log(texto);
+}
+
+urlAmigavel(texto);
