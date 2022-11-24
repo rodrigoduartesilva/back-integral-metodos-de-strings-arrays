9cb1e4059ed4be842dc7bbf7f48f3b115ae779a3
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2022-11-24 20:37:54 -0300

    Classe 06 - Finalizado

--- classe-06/index.js
+++ classe-06/index.js
@@ -1,1 +1,19 @@
 const frutas = ['Banana', 'Maçã', 'Abacaxi', 'Pêra', 'Uva'];
+const frutasInverso = '';
+
+function frutasInversor(frutas) {
+    const frutasInverso = frutas.reverse();
+    return frutas;
+}
+
+function frutasDelAdd(frutas) {
+    frutas.shift();
+    frutas.pop();
+    frutas.push('Melão');
+
+    return frutas;
+}
+
+
+
+console.log(frutasDelAdd(frutas));
