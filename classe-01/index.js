7c2f4fc7372a8fb06e2e7946f7589614a5e178ae
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2022-11-24 14:56:33 -0300

    Classe 01 - Finalizado

--- classe-01/index.js
+++ classe-01/index.js
@@ -1,1 +1,10 @@
 const comentario = "Esse COVID é muito perigoso!";
+
+const uppeCFrase = comentario.toUpperCase();
+
+const verificacao = uppeCFrase.includes('COVID') || uppeCFrase.includes('PANDEMIA');
+
+
+if (verificacao) {
+    console.log('Comentário bloqueado por conter palavras proibidas');
+}
