48ec172145969730a827d83c770c1aef80a4c68d
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2022-11-24 15:27:42 -0300

    Classe 02 - Finalizado

--- classe-02/index.js
+++ classe-02/index.js
@@ -1,2 +1,16 @@
 const cpf = "12345678900";
-const cnpj = "12345678900";
+const cnpj = "12345678000199";
+
+if (cpf.length !== 11) {
+    console.log('CPF Inválido.');
+} else {
+    const cpfFormat = `${cpf.substr(0, 3)}.${cpf.substr(3, 3)}.${cpf.substr(6, 3)}-${cpf.substr(9, 2)}`;
+    console.log(cpfFormat);
+}
+
+if (cnpj.length !== 14) {
+    console.log('CNPJ Inválido.');
+} else {
+    const cnpjFormat = `${cnpj.substr(0, 2)}.${cnpj.substr(2, 3)}.${cnpj.substr(5, 3)}/${cnpj.substr(8, 4)}-${cnpj.substr(12, 2)}`;
+    console.log(cnpjFormat);
+}
