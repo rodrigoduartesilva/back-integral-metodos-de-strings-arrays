f404174a95700e79df1b2019418fb3e444e560ad
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2022-11-24 18:22:37 -0300

    Classe 04 - Finalizado

--- classe-04/index.js
+++ classe-04/index.js
@@ -2,3 +2,35 @@ let identificador = "123";
 let nome = "José silva costa";
 let email = "      jose@email.com  ";
 let tags = ['financeiro', 'administrativo', 'geral'];
+
+function idCorrigido(identificador) {
+    let identificadorCorrigido = identificador.padStart(6, 0);
+    console.log(identificadorCorrigido);
+}
+
+function corrigirNomeMaiuscula(nome) {
+    let nomeCorrigir = nome.split(' ');
+    let primeiraLetraUp = '';
+
+    for (let i = 0; i < nomeCorrigir.length; i++) {
+        let nomeDiv = '';
+
+        nomeDiv = nomeCorrigir[i].split('');
+        primeiraLetraUp += `${nomeDiv[0].toUpperCase() + nomeDiv.slice(1).join('')} `;
+    }
+
+    console.log(primeiraLetraUp.trim());
+}
+
+function emailSemEspaco(email) {
+    console.log(email.trim());
+}
+
+function unirTags(tags) {
+    console.log(tags.join(', '));
+}
+
+idCorrigido(identificador);
+corrigirNomeMaiuscula(nome);
+emailSemEspaco(email);
+unirTags(tags);
