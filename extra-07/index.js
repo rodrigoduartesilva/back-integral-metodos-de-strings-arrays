ff628e0de4c32e16861a2645c333ce4a0a21420c
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2022-11-26 16:53:04 -0300

    Extra 07 - Finalizado

--- extra-07/index.js
+++ extra-07/index.js
@@ -1,1 +1,20 @@
 const celular = 7199918888;
+
+
+function formatarNumeroCelular(celular) {
+    const celularNumeroString = celular.toString();
+    const dddCelular = celularNumeroString.slice(0, 2);
+    const numeroCelSemDDD = celularNumeroString.slice(2);
+    let stringNumCel01 = numeroCelSemDDD.substr(0, 4);
+    let stringNumCel02 = numeroCelSemDDD.substr(4);
+    let numeroRetorno = '';
+
+    if (numeroCelSemDDD.length == !9) {
+        return numeroRetorno = `${9} ${([stringNumCel01, stringNumCel02]).join('-')}`;
+    } else {
+        return numeroRetorno = `(${dddCelular}) ${9} ${([stringNumCel01, stringNumCel02]).join('-')}`;
+    }
+}
+
+
+console.log(formatarNumeroCelular(celular));
