d3f583e99160a99018518cb3e3e18652f0de9217
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2022-11-30 15:48:14 -0300

    Extra 10 - Finalizado

--- extra-10/index.js
+++ extra-10/index.js
@@ -1,1 +1,15 @@
 const cpf = "011.022.033-44";
+
+function removerPontuacao(cpf) {
+    let modificar = cpf
+    let resultado = '';
+
+    while (modificar !== resultado) {
+        resultado = modificar;
+        modificar = modificar.replace('.', '').replace('-', '').replace('/', '');
+    }
+
+    console.log(resultado);
+}
+
+removerPontuacao(cpf);
