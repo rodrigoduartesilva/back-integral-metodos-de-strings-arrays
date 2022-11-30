abc4c3516d9c88cd8b07ba10330bce6aa7052d31
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2022-11-30 15:59:27 -0300

    Extra 11 - Finalizado

--- extra-11/index.js
+++ extra-11/index.js
@@ -1,2 +1,18 @@
 const nomes = ['Ford Ká', 'Ranger', 'Hilux', 'Corola', 'Fusca', 'Chevete', 'Brasilia'];
 const posicao = 3;
+
+function acharCarros(nomes, posicao) {
+    const carrosLocalizados = nomes.slice(posicao, posicao + 3).toString();
+    let modificar = carrosLocalizados;
+    let resultado = '';
+
+    while (modificar !== resultado) {
+        resultado = modificar;
+        modificar = modificar.replace(',', ' - ');
+    }
+
+    console.log(resultado);
+}
+
+
+acharCarros(nomes, posicao);
