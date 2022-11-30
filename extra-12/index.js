17b70019e675f328baeee7c351f9d46b76af24d6
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2022-11-30 16:15:05 -0300

    Extra 12 - Finalizado

--- extra-12/index.js
+++ extra-12/index.js
@@ -1,2 +1,29 @@
-const nomes = ['Juninho', 'Léo', 'Guido', 'Dina', 'Vitinho', 'Nanda'];
-const tamanhoDoGrupo = 4;
+const nome = 'Guido Cerqueira';
+
+function criarNickname(nome) {
+    let modificarNome = nome.toLowerCase();
+    let resultadoMinusculas = '';
+    let sinalArroba = '@';
+    let resultadoFinal = '';
+
+    while (modificarNome !== resultadoMinusculas) {
+        resultadoMinusculas = modificarNome;
+        modificarNome = modificarNome.replace(' ', '');
+    }
+
+    modificarNome = sinalArroba + modificarNome;
+
+    if (modificarNome.length > 13) {
+        resultadoFinal = modificarNome.slice(0, 13);
+        console.log(resultadoFinal);
+    } else {
+        console.log(modificarNome);
+    }
+
+
+}
+
+
+
+
+criarNickname(nome);
