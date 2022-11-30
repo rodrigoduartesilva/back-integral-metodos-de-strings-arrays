abba7e3b43034a414619d7c25214e0dbffdf8b7e
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2022-11-30 16:51:14 -0300

    Extra 13 - Finalizado

--- extra-13/index.js
+++ extra-13/index.js
@@ -1,1 +1,16 @@
 const nomeArquivo = 'Foto da Familia.pdf';
+
+
+function determinarExtensao(nomeArquivo) {
+    const localizarPonto = nomeArquivo.indexOf('.');
+    const localizarExtensao = nomeArquivo.slice(localizarPonto + 1);
+
+    if (localizarExtensao === 'jpg' || localizarExtensao === 'jpeg' || localizarExtensao === 'gif' || localizarExtensao === 'png') {
+        console.log('Arquivo válido');
+    } else {
+        console.log('Arquivo inválido');
+    }
+}
+
+
+determinarExtensao(nomeArquivo);
