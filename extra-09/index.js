d160f1c96484adfa7f20446098e50d7ddaa27741
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2022-11-30 15:28:56 -0300

    Extra 09 - Finalizado

--- extra-09/index.js
+++ extra-09/index.js
@@ -1,2 +1,13 @@
 const nomes = ['Juninho', 'Léo', 'Guido', 'Dina', 'Vitinho', 'Nanda'];
 const tamanhoDoGrupo = 4;
+
+function separarGrupos(nomes, tamanhoDoGrupo) {
+    const grupo1 = nomes.slice(0, tamanhoDoGrupo);
+    const grupo2 = nomes.slice(tamanhoDoGrupo);
+
+    console.log(`Grupo 1: ${grupo1}`);
+    console.log(`Grupo 2: ${grupo2}`);
+}
+
+
+separarGrupos(nomes, tamanhoDoGrupo);
